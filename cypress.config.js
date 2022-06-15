514ed0bb3c3940dd89560b9e8945b64b0e4e512e
baoxian <bxtsang.2018@sis.smu.edu.sg>
2022-06-15 21:14:59 +0800

    Create one component test

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,10 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173'
+    baseUrl: 'http://localhost:3000'
   },
   component: {
-    specPattern: 'src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
+    specPattern: 'src/**/*.{cy,spec}.{js,ts,jsx,tsx}',
     devServer: {
       framework: 'vue',
       bundler: 'vite'
---
+++ src/components/__tests__/ArticleCard.cy.js
@@ -0,0 +1,9 @@
+import ArticleCard from '../ArticleCard.vue'
+
+describe ('Article card component test')
+
+it ('renders card', () => {
+    cy.mount(ArticleCard, { props: { title: 'test' } })
+        .get('h1')
+        .should('contain.text', 'test')
+})
